2f0834f02ae27d124e8403a11871cbb223662807
fessa417 <73225068+Egriss417@users.noreply.github.com>
2024-01-26 11:04:41 +0300

    serviceController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,14 +3,10 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import web.dao.CarDao;
-import web.dao.CarDaoImp;
 import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,8 +16,8 @@ public class CarController {
     public String printCars(HttpServletRequest request, ModelMap model) {
         String count = request.getParameter("count");
         CarService carService = new CarServiceImpl();
-        CarDao carDao = new CarDaoImp();
-        List<Car> cars = carService.printCars(carDao.getAllcar(), count);
+
+        List<Car> cars = carService.printCars((count != null)?Integer.parseInt(count):5);
 
         model.addAttribute("cars", cars);
         return "cars";
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,15 +1,18 @@
 package web.service;
 
+import web.dao.CarDao;
+import web.dao.CarDaoImp;
 import web.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarServiceImpl implements CarService{
-
+    private final CarDao carDao = new CarDaoImp();
     @Override
-    public List<Car> printCars(List<Car> cars, String count) {
-        int length = (count == null || Integer.parseInt(count) >= 5) ? cars.size() : Integer.parseInt(count);
+    public List<Car> printCars(int count) {
+        List<Car> cars = carDao.getAllcar();
+        int length = (count >= 5) ? cars.size() : count;
         return cars.stream().limit(length).collect(Collectors.toList());
     }
 }
